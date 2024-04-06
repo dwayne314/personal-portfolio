edada5215413373026279593f3b1fe699a935e06
dwayne314 <dwayne.manlove@gmail.com>
2024-04-06 01:54:24 -0400

    Add support for Google Analytics

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,25 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+                    page_path: window.location.pathname,
+                  });
+                `,
+          }}
+        />
+      </Head>
       <body className="bg-[#0f1415]">
         <Main />
         <NextScript />
